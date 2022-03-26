41e6b15c15d71ba1e4b89676b9125c370e801d8c
Spencer Miller <spencer.miller@me.com>
2022-03-26 19:52:52 +0300

    Added token URI

--- contracts/Collectible.sol
+++ contracts/Collectible.sol
@@ -6,11 +6,18 @@ import "hardhat/console.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract Collectible is ERC721 {
+    uint256 public tokenCounter;
     
-    constructor() public ERC721("Mountain", "MNT") {}
+    constructor() public ERC721("Mountain", "MTN") {
+        tokenCounter = 0;
+    }
 
-    function createCollectible() public returns (uint256) {
-        
+    function createCollectible(string memory tokenURI) public returns (uint256) {
+        uint256 newTokenId = tokenCounter;
+        _safeMint(msg.sender, newTokenId);
+        _setTokenURI(newTokenId, tokenURI);
+        tokenCounter++;
+        return newTokenId;
     }
 
 }
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,29 +1,21 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// When running the script with `npx hardhat run <script>` you'll find the Hardhat
-// Runtime Environment's members available in the global scope.
 const hre = require("hardhat");
 
+// Beach: https://ipfs.io/ipfs/QmT4wCzX57pCusNwS2wSYA12F9AUSvEieRfUsLoEPAkZ7k?filename=beach.jpg
+// Valley: https://ipfs.io/ipfs/QmW92AkvERx8LFKvomtHxEWdGjCS4EyuDoK72HYZSPPisv?filename=valley.jpg
+
 async function main() {
-  // Hardhat always runs the compile task when running scripts with its command
-  // line interface.
-  //
-  // If this script is run directly using `node` you may want to call compile
-  // manually to make sure everything is compiled
-  // await hre.run('compile');
+  const signer = hre.ethers.getSigner();
 
-  // We get the contract to deploy
-  const Greeter = await hre.ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
+  const Collectible = await hre.ethers.getContractFactory("Collectible");
+  
+  const collectible = await Collectible.deploy();
+  await collectible.deployed();
 
-  await greeter.deployed();
+  await collectible.createCollectible("https://ipfs.io/ipfs/QmeWEax2NKqKy7TnBACPNw2bCYrt1TW2nCc9ujPERcmBcr?filename=mountain.jpg")
 
-  console.log("Greeter deployed to:", greeter.address);
+  console.log("Collectible deployed to:", collectible.address);
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
